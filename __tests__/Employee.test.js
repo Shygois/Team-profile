ca1e6bcc6b70b24c9b462183ca463728897b4e09
Shygois <shayana.gois@att.net>
2021-04-11 10:56:07 -0700

    Updated employee test

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -2,7 +2,43 @@ const Employee = require('../lib/Employee.js');
 
 //Employee parent class with the following properties - name, id, email)
 test('creates Employee object', () => {
-    const employee = new Employee();
+    const employee = new Employee('John Smith', 12345, 'johnsmith@email.com');
 
-    expect(typeof(employee)).toBe('object');
+    expect(typeof (employee)).toBe('object');
+});
+
+test('Employee name is a string', () => {
+    const employee = new Employee('John Smith');
+
+    expect(employee.name).toEqual(expect.any(String));
+});
+
+test('Employee name is John Smith', () => {
+    const employee = new Employee('John Smith');
+
+    expect(employee.name).toBe('John Smith');
+});
+
+test('Employee id is a number', () => {
+    const employee = new Employee('', 12345);
+
+    expect(employee.id).toEqual(expect.any(Number));
+});
+
+test('Employee id is 12345', () => {
+    const employee = new Employee('', 12345);
+
+    expect(employee.id).toBe(12345);
+});
+
+test('Employee email is a string', () => {
+    const employee = new Employee('', 12345, 'johnsmith@email.com');
+
+    expect(employee.email).toEqual(expect.any(String));
+});
+
+test('Employee email is johnsmith@email.com', () => {
+    const employee = new Employee('', 12345, 'johnsmith@email.com');
+
+    expect(employee.email).toBe('johnsmith@email.com');
 });
